12d846b422368a2c797edeaa5e6d2b96fcba8362
Aleksander <aleksandr.gerasimov.05@gmail.com>
2023-04-27 09:31:51 +0700

    Created registration and login logic

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from data.db_session import global_init
+from data.db_session import global_init, create_session
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_login import (
     current_user,
+    login_user,
+    LoginManager, login_required, logout_user,
 )
+
+from data.forms.LoginForm import LoginForm
+from data.forms.RegisterForm import RegisterForm
+from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
 app.config['UPLOAD_FOLDER'] = 'static/media/'
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 5120  # 5 GB
+login_manager = LoginManager()
+login_manager.init_app(app)
 global_init('db/database.db')
 DEBUG = True
+
 
 @app.route('/')
 def main():
@@ -18,6 +27,76 @@ def main():
         'homepage.html',
         title='SpaceEd',
         current_user=current_user,
+    )
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template(
+                'register.html',
+                title='Регистрация',
+                form=form,
+                message="Пароли не совпадают",
+                current_user=current_user,
+            )
+        db_sess = create_session()
+        if (
+            db_sess.query(User)
+            .filter(
+                (User.email == form.email.data) | (User.nickname == form.nickname.data)
+            )
+            .first()
+        ):
+            return render_template(
+                'register.html',
+                title='Регистрация',
+                form=form,
+                message="Такой пользователь уже есть",
+                current_user=current_user,
+            )
+        user = User(nickname=form.nickname.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template(
+        'register.html', title='Регистрация', form=form, current_user=current_user
+    )
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template(
+            'login.html',
+            message="Неправильный логин или пароль",
+            form=form,
+            current_user=current_user,
+        )
+    return render_template(
+        'login.html', title='Авторизация', form=form, current_user=current_user
     )
 
 
